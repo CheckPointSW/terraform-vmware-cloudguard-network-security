e294eb53a7c8ecb6628602482dbb472a107ad143
eviatars <eviatars@checkpoint.com>
2025-11-05 10:19:34 +0000

    change provider to vmware/vsphere

--- modules/single-gateway/versions.tf
+++ modules/single-gateway/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=1.10.05"
   required_providers {
     vsphere = {
-      source  = "hashicorp/vsphere"
+      source  = "vmware/vsphere"
       version = ">2.11.1"
     }
   }
